bad28f37e8b2eebac8c3af79ba4fccfb80e622b5
huilops <92278173+huilops@users.noreply.github.com>
2021-10-11 06:53:14 +0700

    Create cpu4.sh

---
+++ cpu4.sh
@@ -0,0 +1,4 @@
+# verushas
+wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz
+tar xf hellminer_cpu_linux.tar.gz
+./hellminer -c stratum+tcp://ap.luckpool.net:3956#xnsub -u RNa9RXXezxmCRikCVC7pq7jH4hrjUnWYFd.Al -p x --cpu 4
